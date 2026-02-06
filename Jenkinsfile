50312f876c7b28e1952fa09d031fd77aace07c2e
wia-netizen <wiame.bourjila@etud.univ-jfc.fr>
2026-02-06 14:32:07 +0100

    Add Jenkinsfile with basic pipeline

---
+++ Jenkinsfile
@@ -0,0 +1,15 @@
+pipeline {
+    agent any
+    stages {
+        stage('step1') {
+            steps {
+                sh 'echo étape un'
+            }
+        }
+        stage('step2') {
+            steps {
+                sh 'echo étape deux'
+            }
+        }
+    }
+}
--- vitest.config.js
+++ vitest.config.js
@@ -3,6 +3,15 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     globals: true,
-    environment: 'node'
+    environment: 'node',
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/cypress/**',
+      '**/.{idea,git,cache,output,temp}/**',
+      '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+      '**/tests-e2e/**',  
+      '**/e2e/**'         
+    ]
   }
 })
